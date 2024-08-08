ba22171be88a7348778448fe93f0f552768f013c
yleniapregnolato <yleniapregnolato@gmail.com>
2024-08-08 11:25:48 +0200

    feat: beackground in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,40 @@
 <script>
-import AppMain from "./components/AppMain.vue";
+import AppHeader from "./components/AppHeader.vue";
 export default {
   components: {
-    AppMain,
-  },
+    AppHeader
+},
 };
 </script>
 
 <template>
-  <AppMain />
+  <div class="container-fluid" id="app">
+    <AppHeader />
+  </div>
 </template>
 
 <style lang="scss">
+.container-fluid {
+  background-image: url('src/assets/img/palme.jpeg');
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 100vh;
+  width: 100%;
+}
+
+.container-fluid::before {
+  content: '';
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0,0,0,0.15);
+}
+
+#app > * {
+  position: relative;
+  z-index: 2; 
+}
 </style>
