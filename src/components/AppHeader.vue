0485f3e832ed3d2660f56a618537065983cf3714
yleniapregnolato <yleniapregnolato@gmail.com>
2024-08-08 11:32:02 +0200

    fix: ul and h2 in AppHeader.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -8,10 +8,10 @@ export default {
     <div class="container">
         <div class="row">
             <div class="col-8">
-                <h1>Travel-app</h1>
+                <h2>Travel-app</h2>
                 <!-- <img src="../assets/img/logo.png" alt="logo" class="logo-img"> -->
             </div>
-            <div class="col-4">
+            <div class="col-4 justify-content-between">
                 <ul class="d-flex justify-content-between">
                     <li>Home</li>
                     <li>I miei viaggi</li>
@@ -24,6 +24,7 @@ export default {
 
 <style scoped lang="scss">
 .row {
+    padding: 15px;
     color: white;
     font-size: 25px;
     background-color: var(--header-color);
@@ -31,8 +32,6 @@ export default {
     background-color: rgba(0,0,0,0.1);
     .col-8, .col-4 {
         z-index: 2;
-        height: 100px;
-        padding: 20px;
         ul {
             list-style: none;
         }
