50bd6e922d01fddd91d9a68dfbf7722ceb460a80
yleniapregnolato <yleniapregnolato@gmail.com>
2024-08-30 17:35:27 +0200

    fix: AppHeader.vue

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -7,15 +7,14 @@ export default {
 <template>
     <div class="container-fluid">
         <div class="row">
-            <div class="col-8">
+            <div class="col-9">
                 <h2>Travel-app</h2>
                 <!-- <img src="../assets/img/logo.png" alt="logo" class="logo-img"> -->
             </div>
-            <div class="col-4 justify-content-between">
+            <div class="col-3 justify-content-between">
                 <ul class="d-flex justify-content-between">
                     <li>Home</li>
                     <li>I miei viaggi</li>
-                    <li>Contatti</li>
                 </ul>
             </div>
         </div>
@@ -30,7 +29,7 @@ export default {
     background-color: var(--header-color);
     border-radius: 30px;
     background-color: rgba(0,0,0,0.1);
-    .col-8, .col-4 {
+    .col-9, .col-3 {
         z-index: 2;
         ul {
             list-style: none;
