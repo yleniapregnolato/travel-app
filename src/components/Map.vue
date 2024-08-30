dc85b8ce1a0947a5cc1ca5ea477260a7973f0f45
yleniapregnolato <yleniapregnolato@gmail.com>
2024-08-30 12:35:26 +0200

    feat: Map.vue in src/components

---
+++ src/components/Map.vue
@@ -0,0 +1,38 @@
+<script>
+import { onMounted } from "vue";
+import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+
+export default {
+    name: "MapView",
+    setup() {
+        onMounted(() => {
+            const map = L.map("map").setView([45.4642, 9.1900], 13);
+
+            L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+            }).addTo(map);
+
+            L.marker([45.4642, 9.1900])
+                .addTo(map)
+                .bindPopup("Questo è un marker a Milano!")
+                .openPopup();
+        });
+    },
+};
+</script>
+
+<template>
+    <div id="map"></div>
+</template>
+  
+<style scoped lang="scss">
+
+#map {
+    height: 500px;
+    width: 50%;
+    position: relative;
+    bottom: 600px;
+    margin-left: 40px;
+}
+</style>
